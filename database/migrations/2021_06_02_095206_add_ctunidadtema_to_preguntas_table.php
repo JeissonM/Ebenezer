7e44a71aead7bc75522a5b7f22c27ecaa3774574
Jordanj9 <jordan_j9@hotmail.com>
2021-06-15 18:14:36 -0500

    fix de migration 2021_06_02_095206_add_ctunidadtema_to_preguntas_table.php

--- database/migrations/2021_06_02_095206_add_ctunidadtema_to_preguntas_table.php
+++ database/migrations/2021_06_02_095206_add_ctunidadtema_to_preguntas_table.php
@@ -13,7 +13,7 @@ class AddCtunidadtemaToPreguntasTable extends Migration
      */
     public function up() {
         Schema::table('preguntas', function (Blueprint $table) {
-            $table->unsignedBigInteger('ctunidadtema_id')->after('materia_id');
+            $table->unsignedBigInteger('ctunidadtema_id')->after('materia_id')->nullable();
             $table->foreign('ctunidadtema_id')->references('id')->on('ctunidadtemas')->onDelete('cascade');
         });
     }
