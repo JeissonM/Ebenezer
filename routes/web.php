5b126c3bbb1c2fef3795a82abc1ac9ac129ded71
JJeissonM <jeisson.mandon@appsiel.com.co>
2021-06-17 09:27:11 -0500

    Corrección de Storage:link

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,18 @@
   |
  */
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/storage-link', function(){
+    Artisan::call('storage:link');
 });
 
 Auth::routes();
